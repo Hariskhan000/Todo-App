cf5488314bd1e0aaed70036c96fef5b53ded1da4
Muhammad Haris Khan <hariskhan873715@gmail.com>
2024-01-23 11:36:03 +0500

    Updates

--- src/Components/AppName.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import styles from "./AppName";
-const AppName = () => {
-  return <h1 className={styles.todoHeading}>Todo App</h1>;
-};
-export default AppName;
--- src/Components/Welcomemessage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./Welcomemessage";
-
-const Welcomemessage = () => {
-  return <h3 className={styles.welcome}>Todo List Project Add Your Task</h3>;
-};
-
-export default Welcomemessage;
